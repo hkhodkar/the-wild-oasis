fb0320361a99681f3f3508344f308089618d43aa
hatef khodkar <hatef.khodkar@hotmail.com>
2024-05-31 15:04:24 +0400

    feat(styles): add styles for button and global styles to project

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,25 @@
+import GlobalStyled from "./styles/Global-Style";
+import Button from "./ui/Button";
+import Heading from "./ui/Heading";
+import Input from "./ui/Input";
+
 function App() {
   return (
-    <div>
-      Hello world
-    </div>
-  )
+    <>
+      <GlobalStyled />
+      <div>
+        <Heading>Hello world</Heading>
+        <Button>
+          Check In
+        </Button>
+        <Button variations="secondary" size="small">
+          Check Out
+        </Button>
+        <Input placeholder="Number of guests" />
+        <Input placeholder="Number of guests" />
+      </div>
+    </>
+  );
 }
 
-export default App
+export default App;
